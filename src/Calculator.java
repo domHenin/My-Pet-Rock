d769c0a8d01ffefd0a9d04d5f7eb92a0dbde09b2
domHenin <domhenin.517@gmail.com>
2019-01-25 14:27:53 -0500

    secondary commit

---
+++ src/Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator {
+    public int sum(int x, int y) {return x + y; }
+
+    public int subtractNum(int x, int y) { return x - y; }
+
+    public int multiplyNum(int x, int y) { return x * y; }
+
+    public int divideNum(int x, int y) { return x / y; }
+}
+
+
+
+//subtractNum
+//multiplyNum
+//divideNum
